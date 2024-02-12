49d3426985460326d81343fcf3087d3cd4f26499
nate smith <nateksmith@pm.me>
2024-02-11 23:27:27 -0800

    ignore

--- assets/main.js
+++ assets/main.js
@@ -2,8 +2,11 @@
 function main() {
   const lines = document.querySelector("#lines");
   const bp = document.querySelector("#blueprint .linecontainer");
-  for (var i = 0; i < 10; i++) {
+  function addLine() {
     lines.appendChild(bp.cloneNode(true));
+  }
+  for (var i = 0; i < 10; i++) {
+    addLine();
   }
 }
 
