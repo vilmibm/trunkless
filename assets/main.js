d4a741127dd93558b9ff9b7e638fad466d9420b2
nate smith <nateksmith@pm.me>
2024-02-11 23:28:30 -0800

    use template

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,13 @@
 // TODO
-function main() {
+
+function addLine() {
+  const ltp = document.querySelector("#linetmpl");
   const lines = document.querySelector("#lines");
-  const bp = document.querySelector("#blueprint .linecontainer");
-  function addLine() {
-    lines.appendChild(bp.cloneNode(true));
-  }
+  const l = ltp.content.cloneNode(true);
+
+  lines.appendChild(l);
+}
+function main() {
   for (var i = 0; i < 10; i++) {
     addLine();
   }
